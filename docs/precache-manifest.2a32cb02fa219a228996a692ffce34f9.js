c8bedc607bc9986e472853cc127892c03a0e4fda
kelsonbatista <kelsonbatista@gmail.com>
2022-04-01 09:47:31 -0300

    Deployment

---
+++ docs/precache-manifest.2a32cb02fa219a228996a692ffce34f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbc9d0f23f0785a82e30d60dc7275d2",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "8ba84e64528b90aa7f28",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/static/css/main.55d66a38.chunk.css"
+  },
+  {
+    "revision": "e20b8a5c21b65b2c6f04",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/static/js/2.f5fb32fe.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/static/js/2.f5fb32fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba84e64528b90aa7f28",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/static/js/main.9cc7c26c.chunk.js"
+  },
+  {
+    "revision": "6fe3e5e60ad44ae4f8cf",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/static/js/runtime-main.1299341e.js"
+  },
+  {
+    "revision": "0cf7e1d82ef31810e99a733492310ca4",
+    "url": "/projects/17-sd-017-project-starwars-planets-search/static/media/starwars1.0cf7e1d8.png"
+  }
+]);
