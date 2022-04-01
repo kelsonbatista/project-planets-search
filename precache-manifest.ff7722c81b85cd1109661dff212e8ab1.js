459ec9ee12c69d381bc4568a7e42a6c3c0f365d1
kelsonbatista <kelsonbatista@gmail.com>
2022-04-01 11:55:47 -0300

    Updates

---
+++ precache-manifest.ff7722c81b85cd1109661dff212e8ab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40555838dcfa88d194dd0af53cbc36af",
+    "url": "/project-planets-search/index.html"
+  },
+  {
+    "revision": "8ba84e64528b90aa7f28",
+    "url": "/project-planets-search/static/css/main.55d66a38.chunk.css"
+  },
+  {
+    "revision": "e20b8a5c21b65b2c6f04",
+    "url": "/project-planets-search/static/js/2.f5fb32fe.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/project-planets-search/static/js/2.f5fb32fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba84e64528b90aa7f28",
+    "url": "/project-planets-search/static/js/main.9cc7c26c.chunk.js"
+  },
+  {
+    "revision": "fa09090ef9744f7e36cc",
+    "url": "/project-planets-search/static/js/runtime-main.e969bfb9.js"
+  },
+  {
+    "revision": "0cf7e1d82ef31810e99a733492310ca4",
+    "url": "/project-planets-search/static/media/starwars1.0cf7e1d8.png"
+  }
+]);
